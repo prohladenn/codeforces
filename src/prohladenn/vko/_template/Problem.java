fda306844454c685f7356ff49cf5938cbd8d57ab
prohladenn <kovshov.valery@gmail.com>
2024-11-04 13:30:39 +0100

    Update template

--- src/prohladenn/vko/_template/Problem.java
+++ src/prohladenn/vko/_template/Problem.java
@@ -1,58 +1,131 @@
 package prohladenn.vko._template;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Problem {
+    static FastInputReader in = new FastInputReader();
+    static PrintWriter out = new PrintWriter(System.out);
 
-    public static void main(String[] args) {
-        FastScanner fs = new FastScanner();
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = fs.nextInt();
-        while (t-- > 0) {
-            // solution
-        }
-
+    public static void main(String[] args) throws Exception {
+        int t = in.nextInt();
+        while (t-- > 0) solve();
         out.close();
     }
 
-    static class FastScanner {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer("");
+    static void solve() throws Exception {
+        out.println(in.nextLine());
+    }
 
-        String next() {
-            while (!st.hasMoreTokens())
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException ignored) {
-                }
-            return st.nextToken();
+    static class FastInputReader {
+        static BufferedReader reader;
+        static StringTokenizer tokenizer;
+
+        public FastInputReader() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        int nextInt() {
+        String next() throws Exception {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(reader.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+
+        String nextLine() throws Exception {
+            String str;
+            if (tokenizer != null && tokenizer.hasMoreTokens()) {
+                str = tokenizer.nextToken("\n");
+            } else {
+                str = reader.readLine();
+            }
+            return str;
+        }
+
+        int nextInt() throws Exception {
             return Integer.parseInt(next());
         }
 
-        int[] readArray(int n) {
-            int[] a = new int[n];
-            for (int i = 0; i < n; i++)
-                a[i] = nextInt();
-            return a;
-        }
-
-        long nextLong() {
+        long nextLong() throws Exception {
             return Long.parseLong(next());
         }
 
-        long[] readArrayL(int n) {
-            long[] a = new long[n];
-            for (int i = 0; i < n; i++)
-                a[i] = nextLong();
-            return a;
+        double nextDouble() throws Exception {
+            return Double.parseDouble(next());
+        }
+
+        BigInteger nextBigInteger() throws Exception {
+            return new BigInteger(next());
+        }
+
+        BigDecimal nextBigDecimal() throws Exception {
+            return new BigDecimal(next());
+        }
+
+        int[] nextArrayInt(int n) throws Exception {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = nextInt();
+            return arr;
+        }
+
+        long[] nextArrayLong(int n) throws Exception {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) arr[i] = nextLong();
+            return arr;
+        }
+
+        double[] nextArrayDouble(int n) throws Exception {
+            double[] arr = new double[n];
+            for (int i = 0; i < n; i++) arr[i] = nextDouble();
+            return arr;
+        }
+
+        BigInteger[] nextArrayBigInteger(int n) throws Exception {
+            BigInteger[] arr = new BigInteger[n];
+            for (int i = 0; i < n; i++) arr[i] = nextBigInteger();
+            return arr;
+        }
+
+        BigDecimal[] nextArrayBigDecimal(int n) throws Exception {
+            BigDecimal[] arr = new BigDecimal[n];
+            for (int i = 0; i < n; i++) arr[i] = nextBigDecimal();
+            return arr;
+        }
+
+        String[] nextArrayString(int n) throws Exception {
+            String[] arr = new String[n];
+            for (int i = 0; i < n; i++) arr[i] = next();
+            return arr;
+        }
+    }
+
+    static class NumberUtils {
+        static long gcd(long a, long b) {
+            BigInteger bigA = BigInteger.valueOf(a);
+            BigInteger bigB = BigInteger.valueOf(b);
+            return bigA.gcd(bigB).longValue();
+        }
+
+        public static int gcd(int a, int b) {
+            BigInteger bigA = BigInteger.valueOf(a);
+            BigInteger bigB = BigInteger.valueOf(b);
+            return bigA.gcd(bigB).intValue();
+        }
+
+        public static long lcm(long a, long b) {
+            BigInteger bigA = BigInteger.valueOf(a);
+            BigInteger bigB = BigInteger.valueOf(b);
+            return bigA.divide(bigA.gcd(bigB)).multiply(bigB).longValue();
+        }
+
+        public static int lcm(int a, int b) {
+            BigInteger bigA = BigInteger.valueOf(a);
+            BigInteger bigB = BigInteger.valueOf(b);
+            return bigA.divide(bigA.gcd(bigB)).multiply(bigB).intValue();
         }
     }
 }
